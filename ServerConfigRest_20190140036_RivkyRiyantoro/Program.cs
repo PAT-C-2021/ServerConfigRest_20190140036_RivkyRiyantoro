972ac0adbafd707935f686f1e3cbce7f7c78bf48
RivkyRiyantoro <sirivkyriyantoro@gmail.com>
2022-01-03 19:13:12 +0700

    Add project files.

---
+++ ServerConfigRest_20190140036_RivkyRiyantoro/Program.cs
@@ -0,0 +1,32 @@
+﻿using ServiceRest_20190140036_RivkyRiyantoro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_20190140036_RivkyRiyantoro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server is Ready..");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
